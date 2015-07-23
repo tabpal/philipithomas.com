944a5502e2883a6b87cfdf912ebf2305fed64760
tabpal <tabpal@users.noreply.github.com>
2015-07-23 19:09:47 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,399 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Philip I. Thomas Curriculum Vitae</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Curriculum Vitae for Philip I. Thomas">
+    <meta name="author" content="Philip I. Thomas">
+
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <style>
+      body {
+        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+      }
+    </style>
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+  </head>
+
+<body class="preview" data-spy="scroll" data-target=".subnav" data-offset="80">
+
+    <div class="container">
+	
+	<header class="jumbotron">
+		
+    <div class="row">
+	<div class="span12">
+		<h3 style="text-align: center; text-style: italic">Curriculum Vitae for</h3>
+		
+      <h1 style="padding: 10px 0px; text-align: center; font-size: 60px; line-height: 70px; text-transform: lowercase">Philip I. Thomas</h1>
+	<span class="" style="text-align: center;"> 
+		<address>
+		<a href="http://philipithomas.com" target="_blank">www.philipithomas.com</a><br />
+		<a href="mailto:#">mail@philipithomas.com</a><br />
+		</address>
+	</span>
+		</div>
+    </div> 
+
+	</header>
+
+<section id="about">
+      <div class="row">
+
+      <div class="page-header">
+ <h2>About</h2>
+      </div>
+          </div> 
+          <div class="row">
+      
+ 		<div class="span12"><p>
+I graduate this month from Washington University in St. Louis, where I am pursuing a Bachelor of Science degree in Systems Engineering with a second major in Physics and a minor in Electrical Engineering. After graduation I am joining <a href="http://opendns.com">OpenDNS</a> in San Francisco as a Software Engineer. </p>
+
+ </div>
+</div> 
+
+      </section>
+
+      <section id="education">
+            <div class="row">
+
+      <div class="page-header">
+      <h2>Education</h2>
+      </div>
+    </div> <!-- end row -->
+<?php
+position("Washington University in Saint Louis","Saint Louis, MO","Bachelor of Science","Expected 5/13",'
+<ul>
+<li><span style="font-weight: bold">Majors:</span> Systems Engineering, Physics.</li>
+<li><span style="font-weight: bold">Minor:</span> Electrical Engineering.</li> 
+<li><span style="font-weight: bold">Honors:</span> J. Alan and Marcia K. Ritter Scholar.</li>
+<li><span style="font-weight: bold">Relevant Coursework:</span> Signal Analysis, Stochastic Simulation, Operations Research.</li>
+<li><span style="font-weight: bold">Senior Project:</span> <a href="http://scheduling.philipithomas.com">Scheduling Algorithm with Optimization of Employee Satisfaction</a></li>
+</ul>',"");
+position("Case Western Reserve University","Cleveland, OH","Organic chemistry lecture and laboratory","6/10-8/10","","");
+
+?>
+
+      </section>
+      
+      
+      
+      <section id="experience">
+            <div class="row">
+
+      <div class="page-header">
+      <h2>Professional and Leadership Experience </h2>
+      </div>
+    </div> <!-- end row -->
+    <?php
+
+      position('OpenDNS', 'San Francisco, CA', 'Software Engineer', '5/13-Present','','http://opendns.com');
+
+     
+
+    position("Text Reject","Saint Louis, MO","Co-Founder","10/12-Present",'
+	<ul>
+	<li>Designed, programmmed, and marketed an series of rejection-sending phone numbers that publish conversations online.</li>
+	<li>Semifinalist for <a href="http://archgrants.org/">Arch Grants International Startup Competition</a>.</li>
+	<li>Semifinalist in national <a href="http://studentstartupmadness.com">Student Startup Madness</a> competition.</li>
+	</ul>','http://textreject.com');
+
+	
+
+	 position("Sparo Labs","Saint Louis, MO","Software Engineer","1/13-5/13",'
+	<ul>
+	<li>Contract Position.</li>
+	<li>Implemented Ruby on Rails-based application for capturing medical device signal, analyzing the recording with a signal processing algorithm in Matlab, and outputting processed data with <a href="www.highcharts.com">Highcharts</a>.</li>
+	<li>Won 1st place in <a href="http://engineering.wustl.edu/disc-comp.aspx">WUSTL Discovery Competition</a>.</li>
+	</ul>','http://sparolabs.com');
+
+
+	
+	position("Cultivation Capital","Saint Louis, MO","Analyst","3/12-5/13",'
+	<ul>
+	<li>Performed screening of potential investments with a focus on technical analysis. </li>
+	</ul>',"http://cultivationcapital.com");
+	
+	
+	position("Washington University Tech Entrepreneurs (wute)","Saint Louis, MO","Director - wute.vc","9/11-5/13",'
+	<ul>
+	<li>Launched wute.vc, a lecture series connecting students with venture capitalists and seasoned entrepreneurs, resulting in sponsorship by <a href="http://cultivationcaptital.com">Cultivation Capital</a>.</li>
+	<li>Attracted over 200 students to <a href="http://wute.vc/2012/10/press-release-alexis-ohanian-kicks-off-student-run-venture-capital-lecture-series-at-wustl/">first wute.vc speech by Reddit Co-Founder Alexis Ohanian</a> using a paperless marketing campaign.</li>
+	<li>Designed <a href="http://wute.vc" target="_BLANK">wute.vc</a> and re-designed <a href="http://wute.org" target="_BLANK">wute.org</a> websites.</li>
+	<li>Secured entry to the <a href="http://www.rackspacestartups.com/">Rackspace Startup Program</a>, including $3,000 worth of free hosting</li>
+	<li>Ran and managed events with speakers including Twitter Founder Jack Dorsey, Square Founder Jim McKelvey, Venture Capitalist and Author Brad Feld, and Reddit Founder Alexis Ohanian</li>
+	</ul>', "http://wute.org");
+	
+	
+	
+	
+	position("WUSauce","St. Louis, MO","Communication Chair","9/10-5/13",'
+	<ul>
+	<li>Performed with audition-only salsa dance team, taught salsa lessons, and participated in community outreach.</li>
+	<li>Implemented improved website and email marketing system, resulting in 20% increase in annual audition attendance and 200% increase in weekly lesson retention throughout each semester.</li>
+	<li>Managed public relations campaign, including negotiating a <a href="http://www.philipithomas.com/wusauce-tv/">live performance on TV</a></li>
+	<li>Served as Communication Director for annual Dance-Off event in 2011, 2012, and 2013, resulting in over 500 students attending each event.</li>
+	</ul>',"http://wusauce.wustl.edu");
+	
+		position("Washington University in St. Louis Department of Physics","Saint Louis, MO","Teaching Assistant, General Physics I and General Physics II","8/11-5/13","", "");
+
+
+		position("Rackspace","Remote","Technical Blogger","12/12-1/13",'
+	<ul>
+	<li>Contract Position.</li>
+	<li>Wrote articles on Rackspace Cloud Files implementations for the <a href="http://rackspace.com/blog">Official Rackspace Blog</a>.</li>
+	</ul>','http://www.rackspace.com/blog/author/philip-thomas/');
+
+	position("Automation Kit","Saint Louis, MO","Managing Partner","6/12-12/12",'
+	<ul>
+	<li>Equipped over a dozen clients with marketing tools to capture, convert, and cultivate leads automatically.</li>
+	<li><span style="font-weight: bold">Selected Clients:</span> Hostirian, Systematic Revenue, Venture Draft, St. Louis Arch Angels.</li>
+	</ul>',"");
+	
+
+	
+	
+	
+	position("Washington University Student Investment Fund","St. Louis, MO","Communication Chair","1/10-6/12",'
+	<ul>
+	<li>Managed the investment of over $100,000 of the university endowment via qualitative, multiples, and DCF analysis, resulting in  growth exceeding S&P 500 Midcap Index on invested funds.</li>
+	<li>Competed nationally in investment competitions, and was a Finalist in the Spring 2011 <a href="http://miiuic.org/" target="_BLANK">Michigan Interactive Investments Competition</a>.</li>
+	<li><span style="font-style:italic;">Previous Positions:</span> Technology Portfolio Manager (2010), Webmaster (2010-2011), Healthcare Analyst (2010).</li>
+	</ul>',"http://wusif.org");
+	
+	
+	position("Washington University in St. Louis Department of Computer Science","St. Louis, MO","Teaching Assistant, Introduction to Computing Tools: MATLAB","1/11-6/11","","");
+	
+	
+   	position("Washington University School of Medicine in St. Louis","St. Louis, MO","Research Assistant, Dr. Jennifer Lodge Laboratory","9/09-1/12",'
+	<ul>
+	<li>Studied pathogenic fungus <a href="http://en.wikipedia.org/wiki/Cryptococcus_neoformans" target="_BLANK"><span style="font-style:italic;">Cryptococcus neoformans</span></a> with in vitro, in vivo, and computational techniques.</li>
+	<li>Selected Project Experience:
+		<ul>
+			<li>Bioinformatics – Designed, developed, and implemented a scalable <span style="font-style:italic;">C. neoformans</span> Strain Database for collaborative use in the scientific community. </li>
+			<li>Gene research – Studied stress sensors involved with nitrositive stress.</li>
+			<li>Animal testing – Analyzed strain virulence with <span style="font-style"italic;">in vitro</span> studies in mice.</li>
+		<ul>
+
+			
+	</li>
+	</ul>',"");
+
+
+           ?>
+    
+    
+      </section>
+      
+      <section id="technical">
+            <div class="row">
+
+      <div class="page-header">
+      <h2>Technical</h2>
+      </div>
+    </div> <!-- end row -->
+<div class="row">
+
+	
+	<div class="span8">
+<?php
+iprogram('Matlab','90');
+iprogram('PHP','85');
+iprogram('Java','50');
+iprogram('CSS','40');
+iprogram('Python','45');
+iprogram('Ruby','60');
+iprogram('Relational Databases','85');
+iprogram('NoSQL Databases','55'); 
+iprogram('Javascript','20');
+iprogram('Adobe CS6','40');
+iprogram('Git','80');
+iprogram('Unix System Administration','40');
+iprogram('Mathematica','60');
+
+
+
+?>
+</div>
+<div class="span4">
+	<div class="well">
+		<h4>Selected Projects</h4>
+		<hr />
+		<ul>
+		<?php
+			project('Food Trucks Near Me','http://www.foodtrucksnearme.com','');
+			project('Numbers.js Advanced Mathematics Library for Node.js and JavaScript <em>(contributor)</em>','','https://github.com/sjkaliski/numbers.js');
+			project('Computational Analysis of Diffusion as a Stochastic System','','http://github.com/philipithomas/diffusion');
+			project('Text Reject','http://textreject.com','https://github.com/textreject');
+			project('CSE131 Java','','https://github.com/philipithomas/cse131Java');
+			project('Curriculum Vitae','','https://github.com/philipithomas/cv-philipithomas');
+		?>
+		</ul>
+		<a class="pull-right btn btn-small btn-inverse" href="http://github.com/philipithomas">Github Profile <i class="icon-arrow-right icon-white"></i></a><br />
+	</div>
+</div>
+</div>
+      
+      </section>
+      
+      
+      <section id="honors">
+            <div class="row">
+
+      <div class="page-header">
+      <h2>Honors and Skills</h2>
+      </div>
+    </div> <!-- end row -->
+	<div class="row">
+	<div class="span8">
+	<?php
+	
+	extra('Awards','
+		1st place, <a href="http://news.wustl.edu/news/Pages/25373.aspx">2013 WUSTL Discovery Competition</a> (<a href="http://www.sparolabs.com">Sparo Labs</a>)<br />
+		Eagle Scout<br />
+		Honorable Mention at 2012 Startup Weekend Columbia (<a href="http://killthelandline.com">Kill the Landline</a>)<br />
+		Finalist in Spring 2011 Michigan Interactive Investments Competition');
+	extra('Language','Conversational Proficiency in Spanish');
+	extra('Organizations','The Engineers\' Club of St. Louis');
+	extra('Interests','Salsa Dancing<br /><a href="http://beer.philipithomas.com">Craft Beer</a><br />Coffee<br />Rock Climbing');
+	
+	?>
+	</div>
+	<div class="span4">
+	<div class="well">
+		<h4>Selected Press</h4>
+		<hr />
+		<ul>
+			<li>St. Louis Business Journal: <a href="http://www.bizjournals.com/stlouis/blog/BizNext/2012/10/community-voices-philip-thomas-at.html?page=all">Philip Thomas at Startup Weekend </a></li>
+				<li>Fox 2 Now: <a href="http://philipithomas.com/2012/07/food-truck-stl-on-fox-2-news-in-the-morning/">Live Interview with Food Truck Site Founder Philip I. Thomas </a></li>
+				<li>Forbes: <a href="http://www.forbes.com/sites/kaipetainen/2011/11/11/washu-students-provide-teva-some-healthy-love/">WashU Students Provide Teva Some Healthy Love</a></li>
+			<li>St. Louis Business Journal: <a href="http://www.bizjournals.com/stlouis/blog/BizNext/2012/09/wutevc-starts-venture-capital-lecture.html">wute.vc starts venture capital lecture series</a></li>
+			<li>Fox 2 Now: <a href="http://philipithomas.com/2012/10/me-on-fox-2-news-for-text-reject/">Text Reject Founders Demonstrate Website Live on TV</a></li>
+			<li>St. Louis Business Journal: <a href="http://www.bizjournals.com/stlouis/blog/BizNext/2013/05/washington-university-students-win.html">Washington University students win $25,000 for medical device</a></li>
+			</ul>
+		<a class="pull-right btn btn-small btn-inverse" href="http://www.philipithomas.com/press">View Full Press <i class="icon-arrow-right icon-white"></I></a><br />
+	</div>
+	</div>
+	</div>
+
+
+      </section>
+
+      <footer style="padding-top: 20px;">
+	<hr />
+      <p style="text-align: center">&copy; <a href="http://www.philipithomas.com">Philip I Thomas</a> 2013</p>
+      </footer>      
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+
+  </body>
+</html>
+
+<?php
+// =========================================================
+/* 
+
+These are some functions to make uniform sections
+
+*/
+
+function position($company,$city,$job,$dates,$blob,$link) {
+	/*
+	This function is for use in the experience section. It unififes formatting for the various positions. 
+	*/
+	
+	echo '
+		<div class="row" style="padding: 35px 0 4px 0;">
+			<div class="span8"><h4 style="margin: 0">'.$company;
+				if (!empty($link)) {
+					echo ' <a href="'.$link.'" target="_BLANK"><span class="badge">Website <i class="icon-arrow-right icon-white"></i></span></a>';
+				}
+			echo '</h4> 
+			
+			</div>	
+			<div class="span4"><h4 class="pull-right hidden-phone" style="margin: 0">'.$city.'</h4>
+			</div>
+			
+		</div> <!-- end row-->
+			
+		<div class="row">
+			<div class="span6"><span style="font-style:italic; font-size: 15px;">'.$job.'</span></div>	
+			<div class="span6">
+			<span class="visible-phone label">'.$dates.'</span>
+			<span class="hidden-phone pull-right" style="font-style:italic; font-size: 15px;" >'.$dates.'</span>
+			
+			</div>
+		</div> <!-- end row-->
+		';
+		if (!empty($blob)) {
+			echo '
+			<div class="row" style="padding-top: 8px;"><div class="span12">'.$blob.'</div></div>';
+			}	
+			
+}
+
+
+function iprogram($language,$percent) {
+	// Progress bars for technical section
+	echo '
+	<div class="row">
+		<div class="span3"
+		<p><h4 style="text-align: right" class="hidden-phone">'.$language.'</h4></p>
+		<p><span class="badge visible-phone" style="text-align: center">'.$language.'</span></p>
+		</div>
+		<div class="span4">
+	      <div class="progress" style="margin: 10px 0 10px 0; height: 22px;">
+	        <div class="bar" style="width: '.$percent.'%;"></div>
+	      </div>
+	    </div>
+		<div class="span1"></div>
+	</div>
+	';
+	
+}
+
+function project($name,$look,$code) {
+	// Makes lists for displaying projects
+	echo '<li style="padding-top: 12px">';
+	
+	if (!empty($look)) {
+		// link to look at completed project
+		echo '<a href="'.$look.'" target="_BLANK" >'.$name.'</a>';
+	}
+	else {
+		echo $name;
+	}
+	
+	if (!empty($code)) {
+		// link to look at completed project
+		echo ' <a href="'.$code.'" target="_BLANK""><span class="badge badge-info">Code <i class="icon-arrow-right icon-white"></i></span></a>';
+	}
+	echo '</li>';
+}
+
+function extra($title,$blob) {
+	echo '<div class="row" style="padding-top:30px">
+				<div class="span3">
+					<span class="pull-right hidden-phone"><strong>'.$title.'</strong></span>
+					<span class="visible-phone badge">'.$title.'</span>
+				</div>
+				<div class="span5">
+					'.$blob.'
+				</div>
+			</div>';
+				
+}
+?>
